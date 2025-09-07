51b5ceffdce13e124ea07366425716c4a92fdc9d
bruninrko123 <balves@byupathway.edu>
2025-09-06 23:48:34 -0300

    cart bug fixed

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,23 @@
-import { setLocalStorage } from './utils.mjs';
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  var cartItems = getLocalStorage('so-cart') || [];
+
+  if (!Array.isArray(cartItems)) {
+    if (cartItems) {
+      cartItems = [cartItems];
+    }
+    else {
+      cartItems = [];
+    }
+  }
+  cartItems.push(product);
+  
+  setLocalStorage('so-cart', cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
