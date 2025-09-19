2032fec7b27550f0d132962723bfe3753ca69f85
Arkangel111 <147678654+Arkangel111@users.noreply.github.com>
2025-09-18 23:02:20 -0400

    dynamic header/footer trials

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from './ProductData.mjs'
 import ProductList from './ProductList.mjs';
+import loadHeaderFooter from './utils.mjs';
 
 
 const dataSource = new ProductData('tents');
@@ -9,4 +10,6 @@ const listElement = document.querySelector('.product-list');
 const productlist = new ProductList('Tents', dataSource, listElement)
 
 productlist.init();
+
+loadHeaderFooter(loadTemplate, renderWithTemplate);
 
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -42,3 +42,25 @@ export function renderListWithTemplate(templateFn, parentElement, list, position
   parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
 
 }
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+export async function loadHeaderFooter(loadTemplate, renderWithTemplate) {
+  const headerTemplate = await loadTemplate('../partials/header.html');
+  const footerTemplate = await loadTemplate('../partials/footer.html');
+  const headerElement = document.qs('#main-header');  
+  const footerElement = document.qs('#main-footer');
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}  
