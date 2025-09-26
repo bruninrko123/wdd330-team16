56de7cae6f6a8f9d86e92b65b23239a7b7b9bc82
bruninrko123 <balves@byupathway.edu>
2025-09-25 23:01:58 -0300

    total

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,6 +4,29 @@ function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
+
+
+//   if (cartItems) {
+//     let total = 0;
+//     cartItems.map((item) => { 
+//       total += item.FinalPrice * item.quantity;
+//       const p = document.querySelector('.total');
+//       p.classList.remove('hide');
+//       p.innerHTML = `Total: $${total.toFixed(2)}`;
+//     })
+//   }
+// }
+
+
+  if (cartItems) {
+    let total = 0;
+    const p = document.querySelector('.total');
+    cartItems.forEach(item => {
+      total += item.FinalPrice * item.quantity;   
+    });
+    p.innerHTML = `Total: $${total.toFixed(2)}`;
+   
+  }
 }
 
 function cartItemTemplate(item) {
