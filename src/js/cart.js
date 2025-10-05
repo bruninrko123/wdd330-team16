7210ecdebb43840b4ae089bc0571d84421c2c952
Arkangel111 <147678654+Arkangel111@users.noreply.github.com>
2025-10-05 14:07:19 -0400

    x item from cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,17 @@
-import { getLocalStorage} from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
+
+  const removeButtons = document.querySelectorAll('.remove-item');
+  removeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const name = button.dataset.name;
+      removeItem(name);
+    });
+  });
 }
 
 function cartItemTemplate(item) {
@@ -20,9 +28,18 @@ function cartItemTemplate(item) {
   <p class='cart-card__color'>${item.Colors?.[0]?.ColorName}</p>
   <p class='cart-card__quantity'> Qt: ${item.quantity}</p>
   <p class='cart-card__price'>$${item.FinalPrice}</p>
+  <button class="remove-item" data-name="${item.Name}">X</button>
 </li>`;
 
   return newItem;
 }
+
+function removeItem(name) {
+  let cartItems = getLocalStorage('so-cart');
+  cartItems = cartItems.filter(item => item.Name !== name);
+  setLocalStorage('so-cart', cartItems);
+  renderCartContents();
+}
+
 // loadHeaderFooter();
 renderCartContents();
